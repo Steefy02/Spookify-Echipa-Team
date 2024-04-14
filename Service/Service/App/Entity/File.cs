02b7b6d5bf65f5e2ad21ab96ae099149e6ab4f12
TrueJohn <100159036+TrueJohn@users.noreply.github.com>
2024-04-14 22:53:31 +0300

    Adding Service

---
+++ Service/Service/App/Entity/File.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.App.Entity
+{
+    public class File {
+        private string file_path;
+        private string owner_name;
+        private int file_id;
+
+        public File(string file_path, string owner_name, int file_id)
+        {
+            this.file_path = file_path;
+            this.owner_name = owner_name;
+            this.file_id = file_id;
+        }
+
+        public string GetFilePath()
+        {
+            return this.file_path;
+        }
+
+
+        public string GetOwnerName()
+        {
+            return this.owner_name;
+        }
+
+        public int GetFileId()
+        {
+            return this.file_id;
+        }
+
+        public void SetFilePath(string file_path)
+        {
+            this.file_path=file_path;
+        }
+
+
+        public void SetOwnerName(string owner_name)
+        {
+            this.owner_name = owner_name;
+        }
+
+      
+
+
+
+    }
+
+}
---
+++ Service/Service/App/Repository/FileRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Service.App.Repository
+{
+    class FileRepository
+    {
+        private List<Entity.File> fileList = new List<Entity.File>();
+
+        public List<Entity.File> GetFileList()
+        {  
+            return fileList; 
+        }
+        
+        public void AddFile(Entity.File file)
+        { 
+            fileList.Add(file);
+        }
+
+
+
+        public void DeleteFile(int fileId)
+        {
+            foreach (Entity.File file in fileList)
+                if(file.GetFileId() == fileId)
+                    fileList.Remove(file);
+            
+        }
+
+        public void UpdateFile(Entity.File givenFile , int fileId)
+        {
+            for (int i = 0; i < fileList.Count; i++)
+                if (fileList[i].GetFileId() == fileId)
+                    fileList[i] = givenFile;
+
+        }
+
+        public Entity.File GetFileById(int fileId)
+        {
+            for (int i = 0; i < fileList.Count; i++)
+                if (fileList[i].GetFileId() == fileId)
+                    return fileList[i];
+
+            return null;
+        }
+
+
+    }
+}
